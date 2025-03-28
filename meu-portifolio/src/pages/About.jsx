77bd1a14559aa5058a9b552c6a2abbfc106f260f
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2025-03-28 18:42:57 -0300

    update project

--- meu-portifolio/src/pages/About.jsx
+++ meu-portifolio/src/pages/About.jsx
@@ -1,11 +1,28 @@
-// src/pages/About.jsx
 import React from "react";
+import './About.css';
+import PerfilImg from '../assets/foto_perfil.png';
 
 const About = () => {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>Conteúdo da página About.</p>
+    <div className="about-container">
+      {/* Seção de texto */}
+      <div className="about-text">
+        <div className="titles_background">
+          <h2 className="background-title">Sobre Mim</h2>
+          <h1 className="main-title">Sobre Mim</h1>
+        </div>
+        <p className="history">
+          Olá! Meu nome é Seu Nome, sou sua profissão ou interesse. 
+          Tenho paixão por seu foco principal e adoro compartilhar conhecimento e experiências.
+        </p>
+        <p className="p_two"> Tenho paixão por seu foco principal e adoro compartilhar conhecimento e experiências.
+        Tenho paixão por seu foco principal e adoro compartilhar conhecimento e experiências.</p>
+      </div>
+
+      {/* Seção da foto */}
+      <div className="about-image">
+        <img src={PerfilImg} alt="Minha Foto" className="picture" />
+      </div>
     </div>
   );
 };
