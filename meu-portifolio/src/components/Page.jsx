c0c45ec7d3abf784d6060496ddb24857ea1864eb
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2025-03-31 18:12:07 -0300

    UPDATE SERVICES

--- meu-portifolio/src/components/Page.jsx
+++ meu-portifolio/src/components/Page.jsx
@@ -1,11 +1,12 @@
 import React, { Suspense, lazy, useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import ParticlesBackground from "./ParticlesBackground";
-import './Page.css'
+import './Page.css';
 
 // Lazy loading das páginas
 const Home = lazy(() => import("../pages/Home"));
 const About = lazy(() => import("../pages/About"));
+const Services = lazy(() => import("../pages/Services"));
 const Skills = lazy(() => import("../pages/Skills"));
 const Timeline = lazy(() => import("../pages/Timeline"));
 const Projects = lazy(() => import("../pages/Projects"));
@@ -15,65 +16,46 @@ const Page = () => {
   const [visibleSections, setVisibleSections] = useState({
     home: false,
     about: false,
+    services: false,  // Adicionando services
     skills: false,
     timeline: false,
     projects: false,
     contact: false,
   });
 
-  // Função para verificar a visibilidade das seções
   const handleIntersection = (entries) => {
     entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        setVisibleSections((prev) => ({
-          ...prev,
-          [entry.target.id]: true, // Marcar a seção como visível
-        }));
-      } else {
-        setVisibleSections((prev) => ({
-          ...prev,
-          [entry.target.id]: false, // Marcar a seção como invisível
-        }));
-      }
+      setVisibleSections((prev) => ({
+        ...prev,
+        [entry.target.id]: entry.isIntersecting,
+      }));
     });
   };
 
   useEffect(() => {
     const observer = new IntersectionObserver(handleIntersection, {
-      threshold: 0.2, // O conteúdo será considerado visível quando 20% da seção estiver visível
+      threshold: 0.2,
     });
 
-    // Observa as seções
     const sections = document.querySelectorAll("section");
-    sections.forEach((section) => {
-      observer.observe(section);
-    });
+    sections.forEach((section) => observer.observe(section));
 
     return () => {
-      // Limpeza do observer
-      sections.forEach((section) => {
-        observer.unobserve(section);
-      });
+      sections.forEach((section) => observer.unobserve(section));
     };
   }, []);
 
   return (
     <div className="container-global">
-      <ParticlesBackground/>
-      {/* Seções com lazy loading e animação de entrada */}
+      <ParticlesBackground />
+
       <section id="home">
         {visibleSections.home && (
           <Suspense fallback={<div></div>}>
             <motion.div
-              initial={{ x: "100%", opacity: 0 }}  // Começa da direita e invisível
-              animate={{ x: 0, opacity: 1 }}       // Vai para o centro e fica visível
-              transition={{
-                type: "spring",        // Tipo de animação "spring"
-                stiffness: 50,         // Menos rigidez para uma animação mais suave
-                damping: 20,           // Mais desaceleração para suavizar a animação
-                duration: 1.5,         // Duração maior para uma transição mais suave
-                delay: 0.2            // Adiciona um pequeno delay para melhorar a fluidez
-              }}
+              initial={{ x: "100%", opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ type: "spring", stiffness: 50, damping: 20, duration: 1.5, delay: 0.2 }}
             >
               <Home />
             </motion.div>
@@ -85,17 +67,26 @@ const Page = () => {
         {visibleSections.about && (
           <Suspense fallback={<div></div>}>
             <motion.div
-              initial={{ x: "100%", opacity: 0 }}  // Começa da direita e invisível
-              animate={{ x: 0, opacity: 1 }}       // Vai para o centro e fica visível
-              transition={{
-                type: "spring",        // Tipo de animação "spring"
-                stiffness: 50,         // Menos rigidez para uma animação mais suave
-                damping: 20,           // Mais desaceleração para suavizar a animação
-                duration: 1.5,         // Duração maior para uma transição mais suave
-                delay: 0.2            // Adiciona um pequeno delay para melhorar a fluidez
-              }}
+              initial={{ x: "100%", opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ type: "spring", stiffness: 50, damping: 20, duration: 1.5, delay: 0.2 }}
             >
               <About />
+            </motion.div>
+          </Suspense>
+        )}
+      </section>
+
+      {/* Nova Seção Services */}
+      <section id="services">
+        {visibleSections.services && (
+          <Suspense fallback={<div></div>}>
+            <motion.div
+              initial={{ x: "100%", opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ type: "spring", stiffness: 50, damping: 20, duration: 1.5, delay: 0.2 }}
+            >
+              <Services />
             </motion.div>
           </Suspense>
         )}
@@ -105,15 +96,9 @@ const Page = () => {
         {visibleSections.skills && (
           <Suspense fallback={<div></div>}>
             <motion.div
-              initial={{ x: "100%", opacity: 0 }}  // Começa da direita e invisível
-              animate={{ x: 0, opacity: 1 }}       // Vai para o centro e fica visível
-              transition={{
-                type: "spring",        // Tipo de animação "spring"
-                stiffness: 50,         // Menos rigidez para uma animação mais suave
-                damping: 20,           // Mais desaceleração para suavizar a animação
-                duration: 1.5,         // Duração maior para uma transição mais suave
-                delay: 0.2            // Adiciona um pequeno delay para melhorar a fluidez
-              }}
+              initial={{ x: "100%", opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ type: "spring", stiffness: 50, damping: 20, duration: 1.5, delay: 0.2 }}
             >
               <Skills />
             </motion.div>
@@ -125,15 +110,9 @@ const Page = () => {
         {visibleSections.timeline && (
           <Suspense fallback={<div></div>}>
             <motion.div
-              initial={{ x: "100%", opacity: 0 }}  // Começa da direita e invisível
-              animate={{ x: 0, opacity: 1 }}       // Vai para o centro e fica visível
-              transition={{
-                type: "spring",        // Tipo de animação "spring"
-                stiffness: 50,         // Menos rigidez para uma animação mais suave
-                damping: 20,           // Mais desaceleração para suavizar a animação
-                duration: 1.5,         // Duração maior para uma transição mais suave
-                delay: 0.2            // Adiciona um pequeno delay para melhorar a fluidez
-              }}
+              initial={{ x: "100%", opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ type: "spring", stiffness: 50, damping: 20, duration: 1.5, delay: 0.2 }}
             >
               <Timeline />
             </motion.div>
@@ -145,15 +124,9 @@ const Page = () => {
         {visibleSections.projects && (
           <Suspense fallback={<div></div>}>
             <motion.div
-              initial={{ x: "100%", opacity: 0 }}  // Começa da direita e invisível
-              animate={{ x: 0, opacity: 1 }}       // Vai para o centro e fica visível
-              transition={{
-                type: "spring",        // Tipo de animação "spring"
-                stiffness: 50,         // Menos rigidez para uma animação mais suave
-                damping: 20,           // Mais desaceleração para suavizar a animação
-                duration: 1.5,         // Duração maior para uma transição mais suave
-                delay: 0.2            // Adiciona um pequeno delay para melhorar a fluidez
-              }}
+              initial={{ x: "100%", opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ type: "spring", stiffness: 50, damping: 20, duration: 1.5, delay: 0.2 }}
             >
               <Projects />
             </motion.div>
@@ -165,15 +138,9 @@ const Page = () => {
         {visibleSections.contact && (
           <Suspense fallback={<div></div>}>
             <motion.div
-              initial={{ x: "100%", opacity: 0 }}  // Começa da direita e invisível
-              animate={{ x: 0, opacity: 1 }}       // Vai para o centro e fica visível
-              transition={{
-                type: "spring",        // Tipo de animação "spring"
-                stiffness: 50,         // Menos rigidez para uma animação mais suave
-                damping: 20,           // Mais desaceleração para suavizar a animação
-                duration: 1.5,         // Duração maior para uma transição mais suave
-                delay: 0.2            // Adiciona um pequeno delay para melhorar a fluidez
-              }}
+              initial={{ x: "100%", opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ type: "spring", stiffness: 50, damping: 20, duration: 1.5, delay: 0.2 }}
             >
               <Contact />
             </motion.div>
---
+++ meu-portifolio/src/pages/Services.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { motion } from "framer-motion";
+import "./Services.css"; // Importando o CSS
+
+const services = [
+  { 
+    icon: "fas fa-globe", 
+    title: "Web Design", 
+    description: "Criação de sites responsivos e interativos usando HTML, CSS, JavaScript, React e Wordpress."
+  },
+  { 
+    icon: "fas fa-pencil-ruler", 
+    title: "UI/UX", 
+    description: "Desenvolvimento de interfaces intuitivas e experiência do usuário com Figma e Adobe XD."
+  },
+  { 
+    icon: "fas fa-mobile-alt", 
+    title: "Desenvolvimento de Web", 
+    description: "Construção de aplicações modernas com React, Node.js e bancos de dados como Firebase."
+  },
+  { 
+    icon: "fas fa-chart-line", 
+    title: "SEO Google", 
+    description: "Otimização de sites para ranqueamento no Google utilizando práticas de SEO avançadas."
+  },
+];
+
+const Services = () => {
+  return (
+    <section id="services" className="service">
+      <div className="service_title">
+        <h2 className="main-titlee">O que eu ofereço?</h2>
+        <h2 className="background-title-service">SERVIÇOS</h2>
+      </div>
+      <div className="service-offices">
+        {services.map((service, index) => (
+          <motion.div
+            key={index}
+            className="service-info"
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: index * 0.2 }}
+          >
+            <div className="service-icon">
+              <i className={service.icon}></i>
+            </div>
+            <div className="service-text">
+              <p>{service.title}</p>
+            </div>
+            <div className="tooltip-wrapper">
+              <div className="tooltip-arrow"><i className="fa-solid fa-circle-info"></i></div>
+              <div className="service-description">{service.description}</div>
+            </div>
+          </motion.div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Services;
