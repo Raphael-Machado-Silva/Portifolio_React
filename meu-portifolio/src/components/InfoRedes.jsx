ea69d3cf1a8adfc77dc937067dba725075ad726e
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2025-04-23 17:49:59 -0300

    update

--- meu-portifolio/src/components/InfoRedes.jsx
+++ meu-portifolio/src/components/InfoRedes.jsx
@@ -6,9 +6,9 @@ const info_redes = () => {
   return (
     <div className='div_redes'>
       <ul>
-        <li><a href="" className='a_redes obj'><i className="fa-brands fa-github"></i></a></li>
-        <li><a href="" className='a_redes obj'><i className="fa-brands fa-linkedin"></i></a></li>
-        <li><a href="" className='a_redes obj'><i className="fa-regular fa-envelope"></i></a></li>
+        <li><a href="https://github.com/Raphael-Machado-Silva" className='a_redes obj' target='_blank'><i className="fa-brands fa-github"></i></a></li>
+        <li><a href="https://www.linkedin.com/in/raphael-machado-silva-74457a291/" target='_blank' className='a_redes obj'><i className="fa-brands fa-linkedin"></i></a></li>
+        <li><a href="#contact" target='_blank'className='a_redes obj'><i className="fa-regular fa-envelope"></i></a></li>
       </ul>
     </div>
   )
